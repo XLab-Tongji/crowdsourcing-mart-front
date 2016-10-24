4ec0bc3d54661404313a106e03282d8a14a3545d
SHIKUN <skylinelayla2012@gmail.com>
2016-10-24 12:46:07 +0800

    添加用户创建众包需求列表

--- app/scripts/controller/portal/Project.list.controller.js
+++ app/scripts/controller/portal/Project.list.controller.js
@@ -19,8 +19,30 @@ app.controller('ProjectListController', ['$scope', '$state', '$rootScope', 'Aler
             }
 
             ).$promise.then(function(data){
+                if(data.result!=null){
+                var result=data.result;
+                // var project_name_list=new Array;
+                // var project_type_list=new Array;
+                // for(var i=0;i<result.length;i++){
+                //     project_name_list[i]=result[i].project_name;
+                //     project_type_list[i]=result[i].project_type;
 
-                console.log(data);
+                // }
+                // $scope.project_name_list=project_name_list;
+                // $scope.project_type_list=project_type_list;
+                $scope.result=result;
+                
+
+
+                }else{
+                    ToasterTool.error('获取失败','请重试');
+                }
+                
+
+
+
+
+
 
             });
             
