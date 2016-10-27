a6c53cbb6cb119f0f22b9d996c0e998f14aa12fa
SHIKUN <skylinelayla2012@gmail.com>
2016-10-27 17:47:21 +0800

    添加报名者列表详情部分数据绑定

--- app/scripts/controller/portal/Enroll.member.list.js
+++ app/scripts/controller/portal/Enroll.member.list.js
@@ -1,33 +1,34 @@
 
 'use strict';
 
-app.controller('EnrollMemberList', ['$scope', '$state', '$rootScope', '$stateParams','AlertTool', 'ToasterTool',
-    'ProjectFactory', 'SessionFactory', 'SessionService','EnrollService', function ($scope, $state, $rootScope, $stateParams,AlertTool, ToasterTool,
-        ProjectFactory, SessionFactory, SessionService,EnrollService) {
+app.controller('EnrollMemberList', ['$scope', '$state', '$rootScope', '$stateParams', 'AlertTool', 'ToasterTool',
+    'ProjectFactory', 'SessionFactory', 'SessionService', 'EnrollService', function ($scope, $state, $rootScope, $stateParams, AlertTool, ToasterTool,
+        ProjectFactory, SessionFactory, SessionService, EnrollService) {
 
         init();
 
         function init() {
-          getmemberdetail();
+            getmemberdetail();
 
         }
 
-       
+        function getmemberdetail() {
 
-        function getmemberdetail(){
-            
             ProjectFactory.enrollmember().get({
-                'project_id':$stateParams.id
+                'project_id': $stateParams.id
             }).$promise.then(function (data) {
-                if(data.status==200)
-                    console.log(data);
+                if (data.status == 200) {
+                    $scope.memberdata = data.result;
+                } else {
+                    ToasterTool.error("获取失败", "请重试");
+                }
             })
 
 
         }
 
-        
-        
+
+
 
 
 
