e5881784b9939af1caf32d6fb11f6ac1553d11fe
swift14370864 <yzy86075222@126.com>
2017-04-06 19:43:07 +0800

    control

--- app/scripts/controller/portal/estimation/Estimation.controller2.js
+++ app/scripts/controller/portal/estimation/Estimation.controller2.js
@@ -7,14 +7,53 @@ app.controller('EstimationController2', ['$scope', '$state', '$rootScope', 'Aler
         init();
 
         function init() {
-            $scope.nextStep = nextStep;         
-        }
+            $scope.nextStep = nextStep; 
 
-        function nextStep(){
-            $state.go('app.main.estimationform3');
-        }
 
-    }]);
+            $scope.fchat = new Object();
+            $scope.fchat.replies = [{key: 0, value: ""}];
+          // 初始化时由于只有1条回复，所以不允许删除
+          $scope.fchat.canDescReply = false;
+          
+         // 增加回复数
+         $scope.fchat.incrReply = function($index) {
+           $scope.fchat.replies.splice($index + 1, 0,
+                 {key: new Date().getTime(), value: ""});   // 用时间戳作为每个item的key
+             // 增加新的回复后允许删除
+             $scope.fchat.canDescReply = true;
+         }
+         
+          // 减少回复数
+          $scope.fchat.decrReply = function($index) {
+             // 如果回复数大于1，删除被点击回复
+             if ($scope.fchat.replies.length > 1) {
+               $scope.fchat.replies.splice($index, 1);
+           }
+             // 如果回复数为1，不允许删除
+             if ($scope.fchat.replies.length == 1) {
+               $scope.fchat.canDescReply = false;
+           }
+       }
+       
+       $scope.fchat.combineReplies = function() {
+           var cr = "";
+           for (var i = 0; i < $scope.fchat.replies.length; i++) {
+               cr += "#" + $scope.fchat.replies[i].value;
+           }
+           cr = cr.substring(1);
+           $log.debug("Combined replies: " + cr);
+           
+           return cr;
+       }        
+   }
+
+   function nextStep(){
+    $state.go('app.main.estimationform3');
+}
+
+
+
+}]);
 
 
 
